12935e0cb7b2a518e104d79537fd65c04b8b0b2f
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-21 20:39:44 +0200

    create 'top 5 games by sales' graph

--- Program.py
+++ Program.py
@@ -9,3 +9,16 @@ print(df.isnull().sum())
 df=df.dropna()
 print(df.isnull().sum())
 
+topGames=df.sort_values(by=['Global_Sales'], ascending=False).head(5)
+games=topGames['Name']
+sale=topGames['Global_Sales']
+
+plt.bar(games,sale)
+
+plt.title("Top 5 games by sales")
+plt.xlabel("Games")
+plt.ylabel("Sales")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+
+plt.show()
